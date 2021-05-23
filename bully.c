3d3e16b8c143b883e04eeb6aa290f3cc2b35c7bc
Taíne Freitas <tddfreitas@inf.ufpel.edu.br>
2021-05-23 18:55:38 -0300

    Fix weight, and election

--- bully.c
+++ bully.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct{
-    int state;
-    int weight;
-}process;
-
-
-
 //Função random
 int random_number (int max){
     int result;
@@ -16,22 +9,33 @@ int random_number (int max){
 }
 
 void main(){
-    int n, result_random = 0, i, j;
-    process *vector_process;
+    int n, result_random = 0, i, *vector_process, elected_process, isRecieved;
     printf("Entre com o número de processos\n");
     scanf("%d", &n);
 
-    vector_process = malloc(n * sizeof(process));
+    vector_process = malloc(n * sizeof(int));
 
     //Define se o processo está ativo(1) ou dormindo (0)  
     for (i = 0; i < n; i++){
-        vector_process[i].state = random_number(2);
-        vector_process[i].weight = random_number(n);
+        vector_process[i] = 0;
     }
+    elected_process = i;
+    vector_process[elected_process] = 1;
 
-    //Determina em que processo a eleição começa.
-    result_random =  random_number(n);
-    printf("O processo escolhido para começar é: %d\n", result_random);
-
-
+    while(1){
+        for(i = 0; i < elected_process; i++){
+            printf("Enviando mensagem de %d\n", i);
+            //Determina se o processo líder recebeu mensagem ou não
+            isRecieved = random_number(2);
+            if (isRecieved == 0){
+                vector_process[elected_process] = 0;
+                elected_process = elected_process -1;
+                printf("Líder dormiu, o novo processo líder é: %d\n", elected_process);
+                vector_process[elected_process] = 1;
+                i = -1;
+            }else{
+                printf("Líder recebeu a mensagem\n");
+            }
+        }
+    }
 }
