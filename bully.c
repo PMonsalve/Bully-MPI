40723080d332efb37db786013de5767e914dfea4
Taíne Freitas <tddfreitas@inf.ufpel.edu.br>
2021-05-23 17:05:35 -0300

    Fill all process with sleep or active

--- bully.c
+++ bully.c
@@ -9,10 +9,20 @@ int random_number (int max){
 }
 
 void main(){
-    int n, result_random = 0;
+    int n, result_random = 0, *vector_process, i, j;
     printf("Entre com o número de processos\n");
     scanf("%d", &n);
+
+    vector_process = malloc(n * sizeof(int));
+
+    //Define se o processo está ativo(1) ou dormindo (0)  
+    for (i = 0; i < n; i++){
+        vector_process[i] = random_number(2);
+        printf("%d\n", vector_process[i]);
+    }
+
+    //Determina em que processo a eleição começa.
     result_random =  random_number(n);
-    printf("O processo escolhido é: %d\n", result_random);
-    
+    printf("O processo escolhido para começar é: %d\n", result_random);
+
 }
