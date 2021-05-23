4b7e09a937ace6f27bf504dc8c73fb0d8dca61b7
Taíne Freitas <tddfreitas@inf.ufpel.edu.br>
2021-05-23 17:33:28 -0300

    Fill all process with weigth and state

--- bully.c
+++ bully.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+    int state;
+    int weight;
+}process;
+
+
+
 //Função random
 int random_number (int max){
     int result;
@@ -9,20 +16,22 @@ int random_number (int max){
 }
 
 void main(){
-    int n, result_random = 0, *vector_process, i, j;
+    int n, result_random = 0, i, j;
+    process *vector_process;
     printf("Entre com o número de processos\n");
     scanf("%d", &n);
 
-    vector_process = malloc(n * sizeof(int));
+    vector_process = malloc(n * sizeof(process));
 
     //Define se o processo está ativo(1) ou dormindo (0)  
     for (i = 0; i < n; i++){
-        vector_process[i] = random_number(2);
-        printf("%d\n", vector_process[i]);
+        vector_process[i].state = random_number(2);
+        vector_process[i].weight = random_number(n);
     }
 
     //Determina em que processo a eleição começa.
     result_random =  random_number(n);
     printf("O processo escolhido para começar é: %d\n", result_random);
+
 
 }
